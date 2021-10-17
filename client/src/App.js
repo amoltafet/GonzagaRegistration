f8bd9d700bd89341d3190ed6a252f8d405b3a964
amoltafet <moltafet.ahmad2015@gmail.com>
2021-10-16 17:04:16 -0700

    worked

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+// We use Route in order to define the different routes of our application
+import { Route } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navbar from "./components/Navbar";
+import Edit from "./components/edit";
+import Create from "./components/create";
+import RecordList from "./components/recordList";
+import Home from "./components/Home";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Route exact path="/">
+        <RecordList />
+      </Route>
+      <Route path="/edit/:id" component={Edit} />
+      <Route path="/create">
+        <Create />
+      </Route>
+      <Route path="/home">
+        <Home />
+      </Route>
+    </div>
+  );
+};
+
+export default App;
